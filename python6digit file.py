dc483a4823e91cc8839c27fba99d6b4c3270b5d1
dream-devils <60604918+dream-devils@users.noreply.github.com>
2020-03-28 19:36:27 +0530

    Add files via upload

---
+++ python6digit file.py
@@ -0,0 +1,35 @@
+print("enter a no.\n 1) odd \n 2)even")
+n = int(input("enter no."))
+print("enter end no.")
+m = int(input("enter no."))
+if (n%2==0):
+	for i in range(0,m):
+		 for j in range(0,10):
+		 	for k in range(0,10):
+		 		for l in range(0,10):
+		 			for m in range(0,10):
+		 				for o in range(0,10):
+		 					for p in range(0,10):
+		 						for q in range(0,10):
+		 							for r in range(0,10):
+		 								for s in range(0,10):
+		 									print("",i,j,k,l,m,o,p,q,r,s)
+		 					
+		 				
+		 			
+		 		
+		 	
+		 
+else :
+	for i in range(0,m):
+		 for j in range(0,10):
+		 	for k in range(0,10):
+		 		for l in range(0,10):
+		 			for m in range(0,10):
+		 				for o in range(0,10):
+		 					for p in range(0,10):
+		 						for q in range(0,10):
+		 							for r in range(0,10):
+		 								for s in range(0,10):
+		 									print("",i,j,k,l,m,o,p,q,r,s)
+	
